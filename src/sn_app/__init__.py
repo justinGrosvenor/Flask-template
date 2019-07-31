228634d0046aac05e26e244383afe79a539916f0
jgrosvenCS178 <jgrosven@mail.ccsf.edu>
2019-07-31 16:41:35 -0700

    basic app + database connection

--- src/sn_app/__init__.py
+++ src/sn_app/__init__.py
@@ -1,4 +1,15 @@
+
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+Bootstrap(app)
 
 import sn_app.views
+
--- src/sn_app/views.py
+++ src/sn_app/views.py
@@ -1,5 +1,7 @@
+
+from flask import render_template
 from sn_app import app
 
 @app.route('/')
 def index():
-	return 'test text'
+	return render_template('index.html')
