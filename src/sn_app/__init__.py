69bf8907cf4929a4c20c88409c457027784e4de9
jgrosvenCS178 <jgrosven@mail.ccsf.edu>
2019-08-09 17:25:40 -0700

    security expressed on front end

--- src/sn_app/__init__.py
+++ src/sn_app/__init__.py
@@ -7,30 +7,42 @@ from flask_security.forms import RegisterForm
 from flask_security import Security, SQLAlchemySessionUserDatastore, UserMixin, RoleMixin, login_required
 from sn_app.database import db_session, init_db
 from sn_app.models import User, Role
-from flask_mail import Mail
+# from flask_mail import Mail
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SECURITY_REGISTERABLE'] = True
-app.config['SECURITY_REGISTER_URL'] = '/create_account'
-# app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['SECURITY_PASSWORD_SALT'] = "2987djakfh982rhfs9"
+app.config['SECURITY_SEND_REGISTER_EMAIL'] = False
+# app.config['MAIL_SERVER'] = 'localhost'
 # app.config['MAIL_PORT'] = 465
-# app.config['MAIL_USE_SSL'] = True
+# app.config['MAIL_USE_SSL'] = False
 # app.config['MAIL_USERNAME'] = 'username'
 # app.config['MAIL_PASSWORD'] = 'password'
+# app.config['SECURITY_EMAIL_SENDER'] = 'example@gmail.com'
 
+bcrypt = Bcrypt(app)
 db = SQLAlchemy(app)
+# mail = Mail(app)
 Bootstrap(app)
 
 # Setup Flask-Security
 user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
-security = Security(app, user_datastore, register_form="security/register_user.html")
+security = Security(app, user_datastore)
 
-mail = Mail(app)
+
+admin = Admin(app, name='admin')
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Role, db.session))
 
 @app.before_first_request
 def create_user():
--- src/sn_app/views.py
+++ src/sn_app/views.py
@@ -1,9 +1,33 @@
 
-from flask import render_template
-from sn_app import app
+from flask import render_template, url_for, flash, redirect
+from sn_app import app, db, bcrypt
 from flask_security import Security, SQLAlchemySessionUserDatastore, UserMixin, RoleMixin, login_required
+from flask_security.forms import RegisterForm
+from sn_app.models import User, Role
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(email=form.email.data, password=hashed_pw)
+        db.session.add(user)
+        db.session.commit()
+        flash('account created', 'success')
+        return redirect('/login')
+    return render_template('/security/register_user.html')
+
+@app.route('/login')
+def login():
+	return redirect('/')
+
+
+@app.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
